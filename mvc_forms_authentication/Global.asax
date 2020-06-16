68d0f7d855dc03fdd8bb8597dd7ace51ee61b4ee
acp-anjan <paudelanjanchandra@gmail.com>
2020-06-16 12:36:34 +0545

    @anjan dbmodels added using ado.net

---
+++ mvc_forms_authentication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvc_forms_authentication.MvcApplication" Language="C#" %>
---
+++ mvc_forms_authentication/ViewModels/UserViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_forms_authentication.ViewModels
+{
+    public class UserViewModel
+    {
+        [Display(Name = "User Name")]
+        [Required(ErrorMessage = "User Name is requried.")]
+        [Remote("UserNameExists", "Account", ErrorMessage = "Username already exists.")]
+        public string UserName { get; set; }
+
+        [Display(Name = "User Email")]
+        [Required(ErrorMessage = "User Email is requried.")]
+        [Remote("EmailExists", "Account", ErrorMessage = "User Email already exists.")]
+        [EmailAddress(ErrorMessage = "Enter valid email address.")]
+        public string UserEmail { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Password is requried.")]
+        [DataType(DataType.Password)]
+        public string UserPassword { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Required(ErrorMessage = "Confirm Password is requried.")]
+        [System.ComponentModel.DataAnnotations.Compare("UserPassword", ErrorMessage = "Password & Confirm Password should be same.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First Name is requried.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last Name is requried.")]
+        public string LastName { get; set; }
+    }
+}
